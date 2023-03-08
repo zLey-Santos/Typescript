e56a18eb5139b6f09f7de0163070b128eeecdce2
zLey-Santos <wy.santos92@gmail.com>
2023-03-08 16:46:20 +0000

    tipos basicos, object, tuplas e array em typescript

--- dist/js/nindex.js
+++ dist/js/nindex.js
@@ -1,6 +1,34 @@
 "use strict";
-let x = 10;
-let stg = 'Wesley';
+/*let x: number = 10;
 x = 31;
-stg = 'Wesley Santos';
-console.log(stg + x);
+
+console.log(x);
+
+// inferência x annotation
+
+let y = 12; //inferência mais usada porem sucinta
+// y = 12;
+
+let z: number = 11; //annotation deixa mais explicito a declaração*/
+
+//tipos basicos
+let firstName = ' Wesley';
+let age = 31;
+const isAdmin = true;
+// String != string
+console.log(typeof firstName);
+firstName = 'João';
+// console.log(firstName);
+console.log(firstName.toUpperCase());
+
+//object
+const myNumbers = [1, 2, 3];
+console.log(myNumbers);
+console.log(myNumbers.length); // mostra quantos elementos temos dentro do array e seus id
+myNumbers.push(5, 4, 12, 23, 1, 90); // adiciona mai elementos ao array
+myNumbers.sort((a, b) => a - b); // organiza um arrAay
+
+//tuplas
+let myTuple;
+myTuple = [5, 'teste', ['a', 'b']]; //tuple organiza de forma fixa a ordem dos elementos 
+//myTuple = [true, false, 5];
--- src/nindex.ts
+++ src/nindex.ts
@@ -1,9 +1,41 @@
-let x: number = 10;
-let stg: string = 'Wesley';
+/*let x: number = 10;
 x = 31;
 
-stg = 'Wesley Santos'
+console.log(x);
+
+// inferência x annotation
+
+let y = 12; //inferência mais usada porem sucinta
+// y = 12;
+
+let z: number = 11; //annotation deixa mais explicito a declaração*/
 
 
-console.log(stg + x);
+//tipos basicos
+
+let firstName: string = ' Wesley';
+let age: number = 31;
+const isAdmin: boolean = true;
+
+// String != string
+
+console.log(typeof firstName);
+firstName = 'João';
+// console.log(firstName);
+console.log(firstName.toUpperCase());
+//object
+
+const myNumbers: number[] = [1, 2, 3];
+
+console.log(myNumbers);
+console.log(myNumbers.length); // mostra quantos elementos temos dentro do array e seus id
+myNumbers.push(5, 4, 12, 23, 1, 90); // adiciona mai elementos ao array
+myNumbers.sort((a, b) => a - b); // organiza um arrAay
+
+//tuplas
+
+let myTuple: [number, string, string[]];
+
+myTuple = [5, 'teste', ['a', 'b']]; //tuple organiza de forma fixa a ordem dos elementos 
+//myTuple = [true, false, 5];
 
