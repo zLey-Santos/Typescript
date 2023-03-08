82d01b9272efc973e23591b16d78813ce0d5b2d9
zLey-Santos <wy.santos92@gmail.com>
2023-03-08 20:02:29 +0000

    tuple any string != string

--- dist/js/nindex.js
+++ dist/js/nindex.js
@@ -9,8 +9,7 @@ console.log(x);
 let y = 12; //inferência mais usada porem sucinta
 // y = 12;
 
-let z: number = 11; //annotation deixa mais explicito a declaração*/
-
+let z: number = 11; //annotation deixa mais explicito a declaração
 //tipos basicos
 let firstName = ' Wesley';
 let age = 31;
@@ -20,15 +19,25 @@ console.log(typeof firstName);
 firstName = 'João';
 // console.log(firstName);
 console.log(firstName.toUpperCase());
-
 //object
 const myNumbers = [1, 2, 3];
 console.log(myNumbers);
 console.log(myNumbers.length); // mostra quantos elementos temos dentro do array e seus id
 myNumbers.push(5, 4, 12, 23, 1, 90); // adiciona mai elementos ao array
 myNumbers.sort((a, b) => a - b); // organiza um arrAay
-
 //tuplas
 let myTuple;
-myTuple = [5, 'teste', ['a', 'b']]; //tuple organiza de forma fixa a ordem dos elementos 
+myTuple = [5, 'teste', ['a', 'b']]; //tuple organiza de forma fixa a ordem dos elementos
 //myTuple = [true, false, 5];
+*/
+
+// object literals -> {pop: value}
+const user = {
+    name: 'Santos',
+    age: 31,
+};
+console.log(user);
+// console.log(typeof user);
+
+
+
--- src/nindex.ts
+++ src/nindex.ts
@@ -8,7 +8,7 @@ console.log(x);
 let y = 12; //inferência mais usada porem sucinta
 // y = 12;
 
-let z: number = 11; //annotation deixa mais explicito a declaração*/
+let z: number = 11; //annotation deixa mais explicito a declaração
 
 
 //tipos basicos
@@ -36,6 +36,21 @@ myNumbers.sort((a, b) => a - b); // organiza um arrAay
 
 let myTuple: [number, string, string[]];
 
-myTuple = [5, 'teste', ['a', 'b']]; //tuple organiza de forma fixa a ordem dos elementos 
-//myTuple = [true, false, 5];
+myTuple = [5, 'teste', ['a', 'b']]; //tuple organiza de forma fixa a ordem dos elementos
+//myTuple = [true, false, 5]; */
 
+// object literals -> {pop: value}
+
+const user: { name: string; age: number } = { // determina oq sera atribuído no object
+    name: 'Santos',
+    age: 31,
+};
+// não pode adicionar nada alem do que foi atribuído 
+console.log(user);
+// console.log(typeof user);
+
+//any valor que aceita qual quer tipo de dados any
+let a: any = 0;
+a = 'teste'; // não é a melhor opção para se usar o any
+a = true;
+a = [];
